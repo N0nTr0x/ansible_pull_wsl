d5f24be043538fa3e43f69ca04a0890f0ace5c1a
Leon <122740694+N0nTr0x@users.noreply.github.com>
2024-03-06 20:30:55 +0100

    Added echos to the script
    I added some echos to the script so you can see what's happening in the terminal

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 # Update package lists (assuming Debian/Ubuntu)
+echo "Updating package lists..."
 sudo apt update
 
 # Install required packages (pipx and git) silently
+echo "Installing required packages..."
 sudo apt install -y pipx git
 
 # Install Ansible with dependencies through pipx
+echo "Installing Ansible with dependencies..."
 pipx install --include-deps ansible
 
 # Ensure pipx paths are added to the environment
+echo "Ensuring pipx paths are added to the environment..."
 pipx ensurepath
 
 # Pull the Ansible repository from the repository
+echo "Pulling the Ansible repository..."
 /home/wotan/.local/bin/ansible-pull -U https://github.com/N0nTr0x/ansible_pull_wsl.git
+
+echo "Done."
