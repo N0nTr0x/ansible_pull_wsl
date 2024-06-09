51d308d308bf543947e7c45fc1a6538f0447031d
Leon <122740694+N0nTr0x@users.noreply.github.com>
2024-06-09 21:42:34 +0200

    Update bootstrap.sh
    Removed pipx and change default install to ansible

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,20 +4,8 @@
 echo "Updating package lists..."
 sudo apt update
 
-# Install required packages (pipx and git) silently
-echo "Installing required packages..."
-sudo apt install -y pipx git
-
-# Install Ansible with dependencies through pipx
-echo "Installing Ansible with dependencies..."
-pipx install --include-deps ansible
-
-# Ensure pipx paths are added to the environment
-echo "Ensuring pipx paths are added to the environment..."
-pipx ensurepath
-
-# Pull the Ansible repository from the repository
-echo "Pulling the Ansible repository..."
-/home/$USER/.local/bin/ansible-pull -U https://github.com/N0nTr0x/ansible_pull_wsl.git
+# Install required packages
+echo "Installing ansible..."
+sudo apt install -y ansible
 
 echo "Done."
