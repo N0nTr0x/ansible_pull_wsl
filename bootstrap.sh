00869caf0c1ecfd5c1a27fe3195c5b157e721cb9
Leon <122740694+N0nTr0x@users.noreply.github.com>
2024-06-09 21:51:07 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ sudo apt update
 
 # Install required packages
 echo "Installing ansible and git..."
-sudo apt install ansible git --install-suggests -y
+sudo apt install ansible git -y
 
 # Pull the Ansible repository from the repository
 echo "Pulling the Ansible repository..."
