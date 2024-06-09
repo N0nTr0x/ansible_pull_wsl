93f738cfd8e0d262e14c2870cdd79e25a8663ef4
Leon <122740694+N0nTr0x@users.noreply.github.com>
2024-06-09 21:50:02 +0200

    Update bootstrap.sh
    Readded the ansible-pull command

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,11 @@ echo "Updating package lists..."
 sudo apt update
 
 # Install required packages
-echo "Installing ansible..."
-sudo apt install ansible --install-suggests -y 
+echo "Installing ansible and git..."
+sudo apt install ansible git --install-suggests -y
+
+# Pull the Ansible repository from the repository
+echo "Pulling the Ansible repository..."
+/home/$USER/.local/bin/ansible-pull -U https://github.com/N0nTr0x/ansible_pull_wsl.git
 
 echo "Done."
